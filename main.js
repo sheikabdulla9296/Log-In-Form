da8bac37b52b0cac3d5b67622e067e08e156bf21
sheikabdulla9296 <sheikabdulla9296@gmail.com>
2024-08-13 17:33:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const userName = document.getElementById("usrName").value;
+const password = document.getElementById("password").value;
+const clearContent = document.getElementById("parent-root");
+
+function clear() {
+    clearContent.innerHTML='';
+}
+
+if(userName == "prasanna" && password == "prasanna1595"){
+    
+}
